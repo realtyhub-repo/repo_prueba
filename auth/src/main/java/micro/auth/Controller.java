85c408203623949bb1eed732b8990f12cad426fc
yerson-zip <yersonochoa.2005@gmail.com>
2026-08-13 18:53:33 -0500

    feature: auth controller

---
+++ auth/src/main/java/micro/auth/Controller.java
@@ -0,0 +1,17 @@
+package micro.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class Controller {
+
+    @GetMapping("")
+    public String message(){
+        return "Hola auth";
+    }
+
+
+}
